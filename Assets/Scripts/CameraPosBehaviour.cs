2633ba8a994b93d5b0a80ef8cd93c92a06aa8ae7
linqen <avena47@gmail.com>
2018-02-13 16:59:46 -0300

    Make Planet-Camera switching script

---
+++ Assets/Scripts/CameraPosBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosBehaviour : MonoBehaviour {
+
+	void Start () {
+		
+		Collider parentCollider = GetComponentInParent<Collider> ();
+		Transform parentTransform = GetComponentInParent<Transform> ();
+		Vector3 parentBounds = parentCollider.bounds.extents;
+		Vector3 parentPosition = parentTransform.position;
+		transform.position = new Vector3 (parentPosition.x + parentBounds.x,
+			parentPosition.y + parentBounds.y, parentPosition.z + parentBounds.z);
+		transform.Rotate (0f, 90.0f, 0f);
+	}
+	
+}
---
+++ Assets/Scripts/SolarSystemCameraManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystemCameraManager : MonoBehaviour {
+	public Camera mainCamera;
+	public Transform[] planetsCameraPos;
+
+	private int currentPos=0;
+	void Start () {
+		mainCamera.transform.position = planetsCameraPos [0].position;
+		mainCamera.transform.rotation = planetsCameraPos [0].rotation;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			currentPos++;
+			if (currentPos == planetsCameraPos.Length) {
+				currentPos = 0;
+			}
+		}
+		mainCamera.transform.position = planetsCameraPos [currentPos].position;
+		mainCamera.transform.rotation = planetsCameraPos [currentPos].rotation;
+	}
+}
